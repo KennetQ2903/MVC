724e8dda559c574a60966c01227837f077a524c4
KennetQ2903 <kennetquiroz@gmail.com>
2024-10-21 10:39:04 -0600

    feat: mvc pattern

---
+++ src/main/java/views/StudentView.java
@@ -0,0 +1,10 @@
+package views;
+
+public class StudentView {
+    public void display(String studentName, String studentRollNo) {
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+}
+
